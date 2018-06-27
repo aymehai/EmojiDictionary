5ad38e6c3588e5af0cb72610ba5bac22fe9f51a2
MehSwitch <aymehaiDev@protonmail.com>
2018-06-27 13:31:31 -0400

    Created cell subclass called "EmojiTableViewCell"

---
+++ EmojiDictionary/EmojiTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  EmojiTableViewCell.swift
+//  EmojiDictionary
+//
+//  Created by Aymen on 6/27/18.
+//  Copyright © 2018 Aymen. All rights reserved.
+//
+
+import UIKit
+
+class EmojiTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
